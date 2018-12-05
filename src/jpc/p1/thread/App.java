83c806cf387af7ec136effbd19606e39ebf9a163
Benfarhat <benfarhat.elyes@gmail.com>
2018-12-05 23:42:49 +0100

    refactor

--- src/jpc/p1/thread/App.java
+++ src/jpc/p1/thread/App.java
@@ -1,39 +1,100 @@
 package jpc.p1.thread;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 class Countdown extends Thread
 {
     public void run()
     {
-    	int i = 0,
-    		length = 26, 
-    		loop = 4;
+    	int randomNum;
+    	while(true) {
+    		try {
+    			display(2);    			
+    			randomNum = ThreadLocalRandom.current().nextInt(1, 6);
+				Thread.sleep(randomNum * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    	}
+        
+    }
+    
+    private void display(int loop) {
+        int i = 0,
+    		length = 26;
     	boolean augmente = true;
     	StringBuilder str = new StringBuilder();
     	
-        for (int count = 0; count <= (length * loop); count++)
+        for (int count = 0; count <= (length * 2 * loop); count++)
         {
-        	str.setLength(0);
-        	for (int j = 0; j <= (length - i); j++) {
-        		str.append(" ");
-        	}
-        	for (int j = 0; j <= i; j++) {
-        		str.append((char)(j + 65));
-        	}
-        	System.out.println(str + "" +  str.deleteCharAt(str.length() - 1).reverse());
-        	if (i >= length) augmente = false;
-        	if (i == 0) augmente = true;
-        	if (augmente) i++;
-        	else i--;
-        }
+	    	str.setLength(0);
+	    	for (int j = 0; j <= (length - i); j++) {
+	    		str.append(" ");
+	    	}
+	    	for (int j = 0; j <= i; j++) {
+	    		str.append((char)(j + 65));
+	    	}
+	    	System.out.println(str + "" +  str.deleteCharAt(str.length() - 1).reverse());
+	    	if (i >= length) augmente = false;
+	    	if (i == 0) augmente = true;
+	    	if (augmente) i++;
+	    	else i--;
+	    }
+    }
+}
+
+class Dot extends Thread
+{
+    public void run()
+    {
+    	int randomNum;
+    	while(true) {
+    		try {
+    			display(2);
+    			randomNum = ThreadLocalRandom.current().nextInt(1, 6);
+				Thread.sleep(randomNum * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    	}
+        
+    }
+    
+    private void display(int loop) {
+        int i = 0,
+    		length = 26;
+    	boolean augmente = true;
+    	StringBuilder str = new StringBuilder();
+    	
+        for (int count = 0; count <= (length * 2 * loop); count++)
+        {
+	    	str.setLength(0);
+	    	for (int j = 0; j <= (length - i); j++) {
+	    		str.append(" ");
+	    	}
+	    	for (int j = 0; j <= i; j++) {
+	    		str.append(".");
+	    	}
+	    	System.out.println(str + "" +  str.deleteCharAt(str.length() - 1).reverse());
+	    	if (i >= length) augmente = false;
+	    	if (i == 0) augmente = true;
+	    	if (augmente) i++;
+	    	else i--;
+	    }
     }
 }
 
 public class App {
 	public static void main(String[] args) {
-		Thread thread = new Countdown();
-		System.out.println("\nUsing Thread.run()\n");
-		thread.run();
-		System.out.println("\nUsing Thread.start()\n");
-		thread.start(); // Créer un thread d'exécution et exécute la méthode run()
+		Thread threadCD = new Countdown();
+		Thread threadDOT = new Dot();
+		/*
+		System.out.println("\nUsing Thread.run(): " + threadCD.getName() + "\n");
+		threadCD.run();
+		*/
+		System.out.println("\nUsing Thread.start(): " + threadCD.getName() + "\n");
+		threadCD.start(); // Créer un thread d'exécution et exécute la méthode run()
+		System.out.println("\nUsing Thread.start(): " + threadDOT.getName() + "\n");
+		threadDOT.start(); // Créer un thread d'exécution et exécute la méthode run()
 	}
 }
